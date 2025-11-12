d497f52fe961131b1d13936aa0578b419d7bdd62
Haziq <Haziq@DESKTOP-JTE1OBL>
2025-11-12 10:54:37 +0800

    added comments for Internship and InternshipApplication

--- SC2002_InternshipPlacementManagementSystem/src/Project/Internship.java
+++ SC2002_InternshipPlacementManagementSystem/src/Project/Internship.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 
 public class Internship {
-	protected int idCounter= 1;
+	protected int idCounter= 1;		// counter for unique Internship IDs
 	
 	protected String id;
 	protected String title;
@@ -13,14 +13,14 @@ public class Internship {
 	protected String preferredMajor;
 	protected LocalDate openDate;
 	protected LocalDate closeDate;
-	protected String status;		// Pending, Approved, Rejected, Failed
+	protected String status;		// Pending, Approved, Rejected, Fiiled
 	protected String companyName;
 	protected CompanyRepresentative companyRep;
-	protected int slotsAvailable;
+	protected int slotsAvailable; 	// can only accept a max of 10 students
 	protected boolean visibility;
 	
-    private InternshipApplication[] internAppli = new InternshipApplication[30];
-    private int applicationCount = 0;
+    private InternshipApplication[] internAppli = new InternshipApplication[30];		// only allowing a max of 30 students to apply, 30 being an arbitrary number
+    private int applicationCount = 0;													// checks how many students have currently applied
 	
     public Internship(String title, String description, String level,
             String preferredMajor, LocalDate openDate,
@@ -36,58 +36,69 @@ public class Internship {
 		this.closeDate = closeDate;
 		this.companyName = companyName;
 		this.companyRep = companyRep;
-		this.slotsAvailable = Math.max(0, Math.min(10, slots)); // ensure 0-10
-		this.visibility = true;
+//		this.slotsAvailable = Math.max(0, Math.min(10, slots)); // ensure 0-10
+		this.slotsAvailable = 10;	// defaults to 10 slots available
+		this.visibility = true; 	// visible by default
+		this.status = "Pending";	// pending by default when created
     }
     
     
+    // checks if current internship is available for display for specified student
     public boolean isOpenFor(Student s) {
-        if (!this.visibility) {
+    	// if internship visibility is not enabled at all
+    	if (!this.visibility) {
         	return false;
         }
-
+    	
+    	// if status of internship status is still Pending, Rejected, or Filled
         if (!status.equalsIgnoreCase("Approved")) {
         	return false;
         }
         
-        
+        // if the current date is within duration that internship is open
         LocalDate now = LocalDate.now();
         if (now.isBefore(openDate) || now.isAfter(closeDate)) {
         	return false;
         }
         
+        // if current student is eligible based on their year
         if (!s.canApplyForLevel(level)) {
         	return false;
         }
         
+        // if current student matches the preferred major of the internship requirements
         if (!preferredMajor.equalsIgnoreCase(s.getMajor())) {
         	return false;
         }
         
+        // returns true if meets above conditions and there is enough slots available
         return slotsAvailable > 0;
     }
     
     
 	public void addApplication(InternshipApplication appli) {
+		// if amount of applications don't exceed max amount of applications available
+		// add a new application
         if (this.applicationCount < this.internAppli.length) {
             internAppli[this.applicationCount++] = appli;
         }
+        
+        
     }
-
+	
 	public void confirmSlot() {
+		// once slot is confirmed, reduce current available slots
         if (this.slotsAvailable > 0) {
         	this.slotsAvailable--;
         }
         
+        // change status to Filled if reduced to 0
         if (this.slotsAvailable == 0) {
         	status = "Filled";
         }
     }
 	
-	
 	// --------- GETTER & SETTER --------- //
-
-	
     
 	public String getId() {
 		return this.id;
--- SC2002_InternshipPlacementManagementSystem/src/Project/InternshipApplication.java
+++ SC2002_InternshipPlacementManagementSystem/src/Project/InternshipApplication.java
@@ -1,21 +1,60 @@
 package Project;
 
 public class InternshipApplication  {
-	private static int idCounter = 1;
+	private static int idCounter = 1;	 // counter for unique Internship Application IDs
 	
 	protected String id;
 	protected Student student;
 	protected Internship internship;
-	protected String status;
-	protected boolean accepted;
+	protected String status;		// Pending, Successful, Unsuccessful
+	protected boolean accepted; 	// checks current internship application has been accepted by student
 	
 	public InternshipApplication(Student student, Internship internship) {
 		this.id = "APP" + (idCounter++);
 		this.student = student;
 		this.internship = internship;
-		accepted = false;
-		status = "Pending...";
+		this.accepted = false;			// application is defaulted to not being accepted yet
+		this.status = "Pending";		// defaults to "Pending"
 	}
+	
+	public boolean acceptPlacement() {
+		
+		// checks if application is Successful AND not been accepted yet by student
+		if(status.equalsIgnoreCase("Successful") && !accepted) {
+			
+			// update to accept it
+			this.accepted = true;
+			
+			// once accepted, update on the internship side to reduce available slots
+			internship.confirmSlot();
+			
+			// retrieve all application submitted by current student
+			InternshipApplication[] applications = student.getInternshipApplications();
+			
+			// loop through all current student's applications
+			for(InternshipApplication app : applications) {
+				
+				// each student can only accept one internship
+				// set status of all applications aside from the accepted one to "Unsuccessful"
+				if(!app.getId().equals(this.id)) {
+					app.setStatus("Unsuccessful");
+				}
+				
+			}
+			
+			// returns true when placement is accepted
+			return true;
+			
+		}
+		
+		// returns false when application is not successful
+		return false;
+	}
+	
+	
+	
+	
+	// --------- GETTER & SETTER --------- //
 	
 	public String getId() {
 		return this.id;
@@ -27,24 +66,6 @@ public class InternshipApplication  {
 	
 	public void setStatus(String status) {
 		this.status = status;
-	}
-	
-	public boolean acceptPlacement() {
-		if(status.equalsIgnoreCase("Successful") && !accepted) {
-			this.accepted = true;
-			internship.confirmSlot();
-			InternshipApplication[] applications = student.getInternshipApplications();
-			
-			for(InternshipApplication app : applications) {
-				if(!app.getId().equals(this.id)) {
-					app.setStatus("Unsuccessful");
-				}
-				
-			}
-			return true;
-			
-		}
-		return false;
 	}
 	
 
